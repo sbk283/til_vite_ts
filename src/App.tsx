42b445d8a71e52678e64e2b4d1e8d4736ecf36dd
sbkcoding <sbkcoding@gmail.com>
2025-08-25 11:12:32 +0900

    [docs] 프로젝트 초기 기본 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-function App() {
-  const unuse = 1;
-  return <div>App</div>;
-}
+const Sample = (): JSX.Element => {
+  return <div>샘플입니다.</div>;
+};
+const App = (): JSX.Element => {
+  return (
+    <div>
+      <h1>App</h1>
+      <Sample />
+    </div>
+  );
+};
 
 export default App;
