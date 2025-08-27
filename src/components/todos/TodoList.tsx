0fa7af9ab31c555590a71687ff52ac2d67ddd9e4
sbkcoding <sbkcoding@gmail.com>
2025-08-27 12:04:03 +0900

    [docs] context와 useReducer 이해

--- src/components/todos/TodoList.tsx
+++ src/components/todos/TodoList.tsx
@@ -1,31 +1,15 @@
+import { useTodos } from "../../contexts/TodoContext";
 import type { TodoType } from "../../types/TodoType";
 import TodoItem from "./TodoItem";
 
-type TodoListProps = {
-  todos: TodoType[];
-  toggleTodo: (id: string) => void;
-  editTodo: (id: string, editTitle: string) => void;
-  deleteTodo: (id: string) => void;
-};
-
-const TodoList = ({
-  todos,
-  toggleTodo,
-  editTodo,
-  deleteTodo,
-}: TodoListProps): JSX.Element => {
+const TodoList = (): JSX.Element => {
+  const { todos } = useTodos();
   return (
     <div>
       <h2>TodoList</h2>
       <ul>
         {todos.map((item: any) => (
-          <TodoItem
-            key={item.id}
-            todo={item}
-            toggleTodo={toggleTodo}
-            editTodo={editTodo}
-            deleteTodo={deleteTodo}
-          />
+          <TodoItem key={item.id} todo={item} />
         ))}
       </ul>
     </div>
